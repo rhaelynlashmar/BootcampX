374e8141fc4a65144d3b0a54ce2dcba5166d5d7c
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-01-19 15:38:55 -0800

    FIX- corrected plural and non-plural issues in assignment_submissions table

--- Migrations/assignment_submissions.sql
+++ Migrations/assignment_submissions.sql
@@ -9,8 +9,8 @@ CREATE TABLE assignments (
 
 CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignment(id) ON DELETE CASCADE,
-  student_id INTEGER REFERENCES student(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
 );

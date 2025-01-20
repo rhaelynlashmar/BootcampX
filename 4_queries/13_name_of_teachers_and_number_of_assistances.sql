5a646d79a67cc11c735ae07b52845ec0229c45cd
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-01-20 15:22:58 -0800

    CREATE- query to see how many assists every teacher had on a (any) selected cohort

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.id) AS number_of_assistances
+FROM assistance_requests
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;

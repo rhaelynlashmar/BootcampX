7ceb5d2ce3e2d84a93b07584561e7a8498701f35
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-01-26 21:18:12 -0800

    ALTER- changed query to be parameterized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,16 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const cohortName = process.argv[2] || '';
+const limit = process.argv[3] || 5;
+
+pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`
-  )
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`,[`%${cohortName}%`, limit])
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
